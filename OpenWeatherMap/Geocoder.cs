aed31e46a95f88dda5ce4d926cd7ad6be5c9fc09
juan lopez salamanca <juanlopezsalamanca1@gmail.com>
2023-04-10 13:28:12 -0500

    Add project files.

---
+++ OpenWeatherMap/Geocoder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWeatherMap
+{
+    public class Geocoder
+    {
+        public Geocoder() { }
+        public decimal lat { get; set; }
+    }
+}
---
+++ OpenWeatherMap/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using static System.Net.WebRequestMethods;
+
+namespace OpenWeatherMap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var countryCode = "US";
+            int zipCode = 60601;
+            var APIKey = "5c4d5f0106f416d779053927067c8b3d";
+            var geoCoderURL = $"http://api.openweathermap.org/geo/1.0/zip?zip={zipCode},{countryCode}&appid={APIKey}";
+            var georesponse = client.GetAsync(geoCoderURL).Result;
+            double latitude = JObject.Parse(georesponse).Latitude;
+
+
+
+        }
+    }
+}
