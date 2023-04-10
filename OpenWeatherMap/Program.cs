7070857a32b177ac1cb10f514b9e8fef3266e830
juan lopez salamanca <juanlopezsalamanca1@gmail.com>
2023-04-10 13:53:24 -0500

    complete

--- OpenWeatherMap/Program.cs
+++ OpenWeatherMap/Program.cs
@@ -8,14 +8,21 @@ namespace OpenWeatherMap
         static void Main(string[] args)
         {
             var client = new HttpClient();
-            var countryCode = "US";
-            int zipCode = 60601;
-            var APIKey = "5c4d5f0106f416d779053927067c8b3d";
-            var geoCoderURL = $"http://api.openweathermap.org/geo/1.0/zip?zip={zipCode},{countryCode}&appid={APIKey}";
-            var georesponse = client.GetAsync(geoCoderURL).Result;
-            double latitude = JObject.Parse(georesponse).Latitude;
+            Console.WriteLine("Enter a zip code to find out the temperature!");
+            int zipCode = int.Parse(Console.ReadLine());
 
+            string key = System.IO.File.ReadAllText("appsettings.json");
+            string APIkey = JObject.Parse(key).GetValue("DefaultKey").ToString();
 
+            var geoCoderURL = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIkey}";
+            var geoResponse = client.GetStringAsync(geoCoderURL).Result;
+
+            var temp = double.Parse(JObject.Parse(geoResponse)["main"]["temp"].ToString());
+            var low = double.Parse(JObject.Parse(geoResponse)["main"]["temp_min"].ToString());
+            var high = double.Parse(JObject.Parse(geoResponse)["main"]["temp_max"].ToString());
+            var city = JObject.Parse(geoResponse)["name"].ToString();
+            Console.WriteLine($"The temperature in {city} is {temp} fahrenheit");
+            Console.WriteLine($"It will be a low of {low} and a high of {high}");
 
         }
     }
